0bf205be69e9cde554e3e502a7f9086222adad2b
Nehal Patel <patelnehal4u@gmail.com>
2021-11-30 20:06:45 +0530

    css changes

--- resources/views/livewire/upload.blade.php
+++ resources/views/livewire/upload.blade.php
@@ -3,16 +3,16 @@
     <div class="wrapper">
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
+                <span aria-hidden="true">&times;</span>
             </button>
             <strong>File too large, max 1024 KB</strong>
             <ul>
-                <li>
-                    <img src="https://via.placeholder.com/50" alt="">
+                <li class="d-flex p-2 align-items-center">
+                    <img src="https://via.placeholder.com/50" alt="" class="img-thumbnail mr-2">
                     <span>girl1.jpeg (size: 2045kb)</span>
                 </li>
-                <li>
-                    <img src="https://via.placeholder.com/50" alt="">
+                <li class="d-flex p-2 align-items-center">
+                    <img src="https://via.placeholder.com/50" alt="" class="img-thumbnail mr-2">
                     <span>girl2.jpeg (size: 3525kb)</span>
                 </li>
             </ul>
@@ -27,21 +27,22 @@
 
         <div class="uploaded-images">
             <div class="card">
-                <div class="card-body d-flex p-2">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    <img src="https://via.placeholder.com/100" alt="" class="img-thumbnail">
-                    <div class="meta">
-                        <p class="card-text">JPEG</p>
-                        <p class="card-text">165.29 KB</p>
-                        <a class="card-link" href="#">Download</a>
+                <div class="card-body d-flex align-items-center p-2">
+                    <img src="https://via.placeholder.com/100" alt="" class="img-thumbnail align-self-start">
+                    <div class="meta p-2">
+                        <p class="card-text m-0">JPEG</p>
+                        <p class="card-text m-0">165.29 KB</p>
+                        <a class="card-link m-0" href="#">Download</a>
                     </div>
-                    <div class="extras">
+                    <div class="extras p-2 align-self-end">
                         <div class="form-group">
                             <label for="name">Name</label>
                             <input type="text" class="form-control">
                         </div>
+                        <a href="#" class="close">
+                            <span aria-hidden="true">&times;</span>
+                        </a>
+
                     </div>
                 </div>
             </div>
